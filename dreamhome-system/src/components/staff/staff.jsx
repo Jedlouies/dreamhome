58d3bdfb513335d20060b0c9ec765a6b391c2c04
ChemzRegaton <112966149+Jedlouies@users.noreply.github.com>
2026-04-19 17:15:19 +0800

    staff header final

--- dreamhome-system/src/components/staff/staff.jsx
+++ dreamhome-system/src/components/staff/staff.jsx
@@ -37,21 +37,26 @@ function Staff() {
               </button>
 
               {isOpen && (
-                <ul className="dropdown-menu">
+                <div className="dropdown-menu">
                   {branches.map((branch) => (
-                    <ul 
+                    <p 
                       key={branch} 
                       onClick={() => handleSelect(branch)}
                       className="dropdown-item"
                     >
                       {branch}
-                    </ul>
+                    </p>
                   ))}
-                </ul>
+                </div>
               )}
             </div>
             <div className='staff-add-btn'>
-              <button className='add-btn'>Add Staff</button>
+              <button className='add-btn'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                </svg>
+                Add Staff
+              </button>
             </div>
           </div>
         </div>
